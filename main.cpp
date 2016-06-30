0d89d2776e28924e2db33a483092fda93b910b19
Russ Butler <c1728p9@gmail.com>
2016-06-29 21:31:27 -0500

    Add UDP and TCP example
    Create a listening UDP socket when connected to the mesh.  Add commands
    to turn on/off a LED and to connect/close a TCP connection.  Add
    scripts capable of sending these commands.
    
    To turn on/off LEDs these commands can be used:
    'python send_over_udp.py --addr <ipv6 address> --data on'
    'python send_over_udp.py --addr <ipv6 address> --data off'
    
    To create a TCP connection these can be used:
    'python send_over_tcp_host.py 1234'
    'python send_over_udp.py --addr <ipv6 address> --data "connect 1234"'
    'python send_over_udp.py --addr <ipv6 address> --data close'

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+/*
+ * Copyright (c) 2016 ARM Limited. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "mbed.h"
 #include "rtos.h"
 #include "NanostackInterface.h"
+#include "socket_example.h"
 
 #define ATMEL   1
 #define MCR20   2
@@ -57,4 +73,9 @@ int main()
         Thread::wait(500);
 
     printf("connected. IP = %s\n", mesh.get_ip_address());
+
+#if !defined(MESH_MINIMAL_SMALLEST_BUILD)
+    // Start socket example
+    start_socket_example((NetworkInterface *)&mesh);    
+#endif
 }
