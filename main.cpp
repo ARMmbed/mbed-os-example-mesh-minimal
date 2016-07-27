88afc4a7a8e084c35b7b8be7f5c73ffe0bcd769b
Seppo Takalo <seppo.takalo@arm.com>
2016-07-27 13:13:50 +0300

    Clean up printf().

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ Serial output(USBTX, USBRX);
 int main()
 {
     output.baud(115200);
-    output.printf("\r\n\r\nConnecting...\r\n");
+    printf("\r\n\r\nConnecting...\r\n");
 
     if (mesh.connect()) {
-        output.printf("Connection failed!\r\n");
+        printf("Connection failed!\r\n");
         return -1;
     }
 
-    output.printf("connected. IP = %s\r\n", mesh.get_ip_address());
+    printf("connected. IP = %s\r\n", mesh.get_ip_address());
 }
