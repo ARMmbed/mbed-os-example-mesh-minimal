a5a5bc5378c8c18e1da399d8acd6b9cb22b7bdcf
Hasnain Virk <hasnain.virk@arm.com>
2016-08-02 00:23:27 +0300

    Adding trace
    * Adding option to enable mbed trace.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,25 @@
 #include "rtos.h"
 #include "NanostackInterface.h"
 
+#if MBED_CONF_APP_TRACE
+#include "mbed-trace/mbed_trace.h"
+
+void trace_printer(const char* str) {
+    printf("%s\r\n", str);
+}
+#endif // MBED_CONF_APP_TRACE
+
 LoWPANNDInterface mesh;
+//ThreadInterface mesh;
 Serial output(USBTX, USBRX);
 
 int main()
 {
+
+#if MBED_CONF_APP_TRACE
+	mbed_trace_init();
+    mbed_trace_print_function_set(trace_printer);
+#endif
     output.baud(115200);
     printf("\r\n\r\nConnecting...\r\n");
 
