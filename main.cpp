3eea347a03228c6b1ea8420eaaa36c06452c5523
Hasnain Virk <hasnain.virk@arm.com>
2016-09-27 15:02:52 +0300

    [ONME-2808] Runtime RF Driver Integration
    * mbed_app.json can be used to select a particular driver.
    * .lib file for mcr20a added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include "rtos.h"
 #include "NanostackInterface.h"
 
+#define ATMEL   1
+#define MCR20   2
+
+#define MESH_LOWPAN     3
+#define MESH_THREAD     4
+
+
 #if MBED_CONF_APP_TRACE
 #include "mbed-trace/mbed_trace.h"
 
@@ -10,8 +17,23 @@ void trace_printer(const char* str) {
 }
 #endif // MBED_CONF_APP_TRACE
 
+
+#if MBED_CONF_APP_RADIO_TYPE == ATMEL
+#include "NanostackRfPhyAtmel.h"
+NanostackRfPhyAtmel rf_phy(ATMEL_SPI_MOSI, ATMEL_SPI_MISO, ATMEL_SPI_SCLK, ATMEL_SPI_CS,
+                           ATMEL_SPI_RST, ATMEL_SPI_SLP, ATMEL_SPI_IRQ, ATMEL_I2C_SDA, ATMEL_I2C_SCL);
+#elif MBED_CONF_APP_RADIO_TYPE == MCR20
+#include "NanostackRfPhyMcr20a.h"
+NanostackRfPhyMcr20a rf_phy(MCR20A_SPI_MOSI, MCR20A_SPI_MISO, MCR20A_SPI_SCLK, MCR20A_SPI_CS, MCR20A_SPI_RST, MCR20A_SPI_IRQ);
+#endif //MBED_CONF_APP_RADIO_TYPE
+
+
+#if MBED_CONF_APP_MESH_TYPE == MESH_LOWPAN
 LoWPANNDInterface mesh;
-//ThreadInterface mesh;
+#elif MBED_CONF_APP_MESH_TYPE == MESH_THREAD
+ThreadInterface mesh;
+#endif //MBED_CONF_APP_MESH_TYPE
+
 Serial output(USBTX, USBRX);
 
 int main()
@@ -23,6 +45,7 @@ int main()
 #endif
     output.baud(115200);
     printf("\r\n\r\nConnecting...\r\n");
+    mesh.initialize(&rf_phy);
 
     if (mesh.connect()) {
         printf("Connection failed!\r\n");
